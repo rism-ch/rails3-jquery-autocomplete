ace4383317300fdc84f28ee3e5e199fdb88e736c
Rodolfo Zitellini <rwz@xhero.org>
2024-05-13 16:36:00 +0200

    Add more option passing

--- lib/rails3-jquery-autocomplete/orm/getter_function.rb
+++ lib/rails3-jquery-autocomplete/orm/getter_function.rb
@@ -14,11 +14,14 @@ module Rails3JQueryAutocomplete
         limit          = get_autocomplete_limit(options)
         #order          = sunspot_get_autocomplete_order(method, options)
         
-        results = send(options[:getter_function], term)
-
+        getter_options = options.include?(:getter_options) ? options[:getter_options] : {}
+        # Override the method name so we can change the autocomplete name
+        record_field = options.include?(:record_field) ? options[:record_field] : method
+        results = send(options[:getter_function], term, getter_options)
+        
         return results.map do |r| 
           label = r.send(:getter_function_autocomplete_label, r)
-          {id: r.id, :"#{method}" => r[method], label: label}
+          {id: r.id, :"#{method}" => r[record_field], label: label}
         end
       end
     end
